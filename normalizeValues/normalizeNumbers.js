98ba49d2ac43ca6399b8a704d3ea5545f28b32f6
Jonathan <jonathan.leritte@gmail.com>
2019-03-19 09:35:27 -0700

    Make Code less Verbose

--- normalizeValues/normalizeNumbers.js
+++ normalizeValues/normalizeNumbers.js
@@ -1,56 +1,47 @@
 // This function takes an array of numbers and will normalize it between twe given values
-function normal(nums,y1,y2,dp) {
-    var tmp = [],i,dem = 1;
-    if(nums && nums.length > 0) {
-        for(i = 0; i<nums.length; i++) {
-            tmp.push(nums[i]);
-        }
-    } else {
-        return -1;
-    }
-    for(i = 0; i<dp;i++){
-        dem *= 10;
-    }
-    if(tmp.length === 1){
-        tmp[0] = y2;
-    }
-    else{
-        var max = Math.max.apply(Math,tmp),
-            min = Math.min.apply(Math,tmp),
-            m, b;
-        m = (y2 - y1) / (max - min);
-        b = y2 - max * m;
-        for(i = 0;i<tmp.length;i++){
-            tmp[i] = Math.round((m * tmp[i] + b)*dem)/dem;
-        }
-    }
-    return tmp.sort(function (a,b) {
-        return a - b;
-    });
+function normal(nums = [],y1,y2,dp = 0) {
+    let dem = 10 ** dp,
+        max = Math.max.apply(Math,nums),
+        min = Math.min.apply(Math,nums), 
+        m = (y2 - y1) / (max - min),
+        b = y2 - max * m
+
+    // if(!nums || nums.length === 0) {
+    //     return -1
+    // }
+    // if(nums.length === 1){
+    //     return [y2]
+    // }
+    // else{
+    //     return nums.map(num => Math.round((m * num + b) * dem) / dem).sort((a,b) => a - b)
+    // }
+    return (!nums || nums.length === 0) ? -1
+            : nums.length === 1 ? [y2]
+            : nums.map(num => Math.round((m * num + b) * dem) / dem).sort((a,b) => a - b)
 }
 
 // Demos of above function
 var test = [1,2,3,4,5,6,7,8,9,10],
     test2 = [],
-    test3;
+    test3
 
-for (i = 0; i<10; i++){
-    test2.push(Math.round(Math.random() * Date.now()));
+while(test2.length < 10){
+    test2.push(Math.round(Math.random() * Date.now()))
 }
-console.log("Call with Random numbers seeded with Date.now().");
-console.log(normal(test2,1,10,0));
-console.log(normal(test2,1,10,1));
-console.log(normal(test2,1,10,2));
-console.log(normal(test2,1,10,3));
+console.log("Call with Random numbers seeded with Date.now().")
+console.log(normal(test2,1,10))
+console.log(normal(test2,1,10,1))
+console.log(normal(test2,1,10,2))
+console.log(normal(test2,1,10,3))
 
-test3 = normal(test2,1,10,3);
+test3 = normal(test2,1,10,3)
 
-console.log("Call with test array [1,2,3,4,5,6,7,8,9,10].");
-console.log(normal(test,1,10,2));
-console.log("Test to make sure normalization is good.");
-console.log(normal(test3,1,10,3));
-console.log("Test single element");
+console.log("Call with test array [1,2,3,4,5,6,7,8,9,10].")
+console.log(normal(test,1,10,2))
+console.log("Test to make sure normalization is good.")
+console.log(normal(test3,1,10,3))
+console.log("Test single element")
 console.log(normal([4],1,10))
-console.log("Return if bad array");
-console.log(normal(null,1,10,2));
-console.log(normal([],1,10,2));
+console.log("Return if bad array")
+console.log(normal(null,1,10,2))
+console.log(normal([],1,10,2))
--- recrusiveNumbers/recrusiveNumFuncs.js
+++ recrusiveNumbers/recrusiveNumFuncs.js
@@ -1,45 +1,47 @@
-console.log(fact(0));
-console.log(fact(1));
-console.log(fact(2));
-console.log(fact(3));
-console.log(fact(4));
-console.log(fact(5));
-console.log(fact(6));
-console.log(fact(7));
-console.log(fact(8));
-console.log(fact(9));
-console.log(fact(10));
+console.log('fact(0):',fact(0))
+console.log('fact(1):',fact(1))
+console.log('fact(2):',fact(2))
+console.log('fact(3):',fact(3))
+console.log('fact(4):',fact(4))
+console.log('fact(5):',fact(5))
+console.log('fact(6):',fact(6))
+console.log('fact(7):',fact(7))
+console.log('fact(8):',fact(8))
+console.log('fact(9):',fact(9))
+console.log('fact(10):',fact(10))
 
 
-console.log(fib(0));
-console.log(fib(1));
-console.log(fib(2));
-console.log(fib(3));
-console.log(fib(4));
-console.log(fib(5));
-console.log(fib(6));
-console.log(fib(7));
-console.log(fib(8));
-console.log(fib(9));
-console.log(fib(10));
+console.log('fib(0):',fib(0))
+console.log('fib(1):',fib(1))
+console.log('fib(2):',fib(2))
+console.log('fib(3):',fib(3))
+console.log('fib(4):',fib(4))
+console.log('fib(5):',fib(5))
+console.log('fib(6):',fib(6))
+console.log('fib(7):',fib(7))
+console.log('fib(8):',fib(8))
+console.log('fib(9):',fib(9))
+console.log('fib(10):',fib(10))
 
 
 // Function for Fibonacci numbers
 function fib(x) {
-	if (x === 0 ){
-		return x;
-	} else if (x <= 2){
-		return 1;
-	} else {
-		return fib(x - 1) + fib(x - 2);
-	}
-};
+	// if (x === 0 ){
+	// 	return x
+	// } else if (x <= 2){
+	// 	return 1
+	// } else {
+	// 	return fib(x - 1) + fib(x - 2)
+	// }
+	return x === 0 ? x : x <= 2 ? 1 : fib(--x) + fib(--x)
+}
 
 // Function for Calculating Factorals
 function fact(x) {
-	if (x === 0){
-		return 1;
-	} else {
-		return x * fact(x - 1);
-	}
-};
+	// if (x === 0){
+	// 	return 1
+	// } else {
+	// 	return x * fact(x - 1)
+	// }
+	return x === 0 ? 1 : x * fact(--x)
+}
